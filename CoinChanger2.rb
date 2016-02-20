3f46f9d3a15de316839ad85ce2571623c6b3d015
Marco-John <john.throckmorton4@gmail.com>
2016-02-20 15:53:31 -0500

    Test for Change of 10

--- CoinChanger2.rb
+++ CoinChanger2.rb
@@ -1,7 +1,10 @@
 def change(change_due)
   coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
 
-  if change_due >= 5
+  if change_due >= 10
+   coins["dimes"] = 1
+   coins["pennies"] = change_due - 10
+  elsif change_due >= 5
     coins["nickles"] = 1
     coins["pennies"] = change_due - 5
   else 
