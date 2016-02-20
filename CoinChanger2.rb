82fa6459f8a5cd5727060be48c489f7c26161023
Marco-John <john.throckmorton4@gmail.com>
2016-02-20 16:59:50 -0500

    Completed each coin value w/ while loop

--- CoinChanger2.rb
+++ CoinChanger2.rb
@@ -1,29 +1,28 @@
 def change(change_due)
   coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
-quarters = 25
-dimes = 10
-nickles = 5
-pennies = 1
+  coin_values = {"quarters" => 25, "dimes" => 10, "nickles" => 5, "pennies" => 1}
 
-  while change_due >= quarters
-    coins["quarters"] = coins["quarters"] + 1
-    change_due = change_due - quarters
+  coin_values.each do |coin, value|
+   
+    while change_due >= value
+     coins[coin] = coins[coin] + 1
+     change_due = change_due - value
+    end
   end
-  
-  while change_due >= dimes
-    coins["dimes"] = coins["dimes"] + 1
-    change_due = change_due - dimes
-  end
-  
-  while change_due >= nickles
-    coins["nickles"] = coins["nickles"] + 1
-    change_due = change_due - nickles
-  end
-  
-  while change_due >= pennies
-     coins["pennies"] = coins["pennies"] + 1
-     change_due = change_due - pennies
-  end
+  #while change_due >= dimes
+  #  coins["dimes"] = coins["dimes"] + 1
+  #  change_due = change_due - dimes
+  #end
+  #
+  #while change_due >= nickles
+  #  coins["nickles"] = coins["nickles"] + 1
+  #  change_due = change_due - nickles
+  #end
+  #
+  #while change_due >= pennies
+  #   coins["pennies"] = coins["pennies"] + 1
+  #   change_due = change_due - pennies
+  #end
     
   coins
   
