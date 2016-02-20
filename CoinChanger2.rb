aa11003622910ffde32be4568b3b9461124425b2
Marco-John <john.throckmorton4@gmail.com>
2016-02-20 15:19:46 -0500

    Test 3 Passing

--- CoinChanger2.rb
+++ CoinChanger2.rb
@@ -1,3 +1,3 @@
 def change(change_due)
-  coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 1}
+  coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => change_due}
 end
--- TestCoinChanger2.rb
+++ TestCoinChanger2.rb
@@ -8,4 +8,7 @@ class TestCoinChanger2 < MiniTest::Test
         assert_equal({"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 1}, change(1))
     end
       
+    def test_change_due_of_3
+        assert_equal({"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 3}, change(3))
+    end 
 end
