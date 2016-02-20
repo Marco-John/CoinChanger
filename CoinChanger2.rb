0613eb57c77c33fef13837b0abe5c84ba03bd209
Marco-John <john.throckmorton4@gmail.com>
2016-02-20 15:37:06 -0500

    Test 5 Passing

--- CoinChanger2.rb
+++ CoinChanger2.rb
@@ -1,3 +1,13 @@
 def change(change_due)
-  coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => change_due}
+  coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
+
+  if change_due >= 5
+    coins["nickles"] = 1
+  else
+    coins["pennies"] = change_due
+  
+  end
+  
+  coins
+
 end
--- TestCoinChanger2.rb
+++ TestCoinChanger2.rb
@@ -7,8 +7,11 @@ class TestCoinChanger2 < MiniTest::Test
     def test_change_due_of_1
         assert_equal({"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 1}, change(1))
     end
-      
     def test_change_due_of_3
         assert_equal({"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 3}, change(3))
-    end 
+    end  
+    def test_change_due_of_5
+        assert_equal({"quarters" => 0, "dimes" => 0, "nickles" => 1, "pennies" => 0}, change(5))
+    end
+    
 end
